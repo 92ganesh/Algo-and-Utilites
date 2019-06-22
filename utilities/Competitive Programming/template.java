fa6604df229c0ae2990363657293d9178e6a8155
Ganesh Patra <ganesh.patra@somaiya.edu>
2019-06-22 08:32:21 +0530

    Update template.java

--- utilities/Competitive Programming/template.java
+++ utilities/Competitive Programming/template.java
@@ -1,15 +1,15 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 class template {
     static BufferedReader br;
     public static void main(String[] args) throws Exception {
         try {
-            br = new BufferedReader(new FileReader("G:\\Warehouse\\custom.txt"));
+            br = new BufferedReader(new FileReader("G:\\Workspace\\codechef\\custom.txt"));     debug=0;
         } catch (Exception e) {
-            br = new BufferedReader(new InputStreamReader(System.in));
+            br = new BufferedReader(new InputStreamReader(System.in));      debug=0;
         }
+        StringBuilder sb = new StringBuilder();
 
         int t = ri();
         while(t-- >0){
@@ -21,15 +21,42 @@ class template {
     }
 
 
-    /* TEMPLATE CREATED BY GANESH PATRA ON 05 March,2019*/
-    private static int ri() throws Exception{           return Integer.parseInt(br.readLine()); }
-    private static int ri(String s){                    return Integer.parseInt(s);             }
-    private static String rs() throws Exception{        return br.readLine();                   }
-    private static String[] rss() throws Exception{     return br.readLine().split(" ");   }
-    private static void pf(Object ob){ System.out.print(ob); }
-    private static void pfn(Object ob){System.out.println(ob); }
+
+
+
+
+
+
+    //****************  Helper functions template from  ******************
+    public static int MOD = (int)1e9+7;
+    private static int ri() throws Exception{           return Integer.parseInt(br.readLine().trim());}
+    private static long rl() throws Exception{          return Long.parseLong(br.readLine().trim());}
+    private static int ri(String s){                    return Integer.parseInt(s);}
+    private static long rl(String s){                   return Long.parseLong(s);}
+    private static String rs() throws Exception{        return br.readLine();}
+    private static String[] rss() throws Exception{     return br.readLine().trim().split("\\s+");}
+    private static void pf(Object ob){ System.out.print(ob);}
+    private static void pfn(Object ob){System.out.println(ob);}
+    private static void pfn(){System.out.println();}
     private static void pfn(int[] arr){
-        for(int i=0;i<arr.length;i++){  System.out.print(arr[i]+" ");   }
-        System.out.println();
+        StringBuilder temp = new StringBuilder();
+        for(int i=0;i<arr.length;i++){  temp.append(arr[i]).append(' ');}
+        System.out.println(temp);
+    }
+    private static void pfn(long[] arr){
+        StringBuilder temp = new StringBuilder();
+        for(int i=0;i<arr.length;i++){  temp.append(arr[i]).append(' ');  }
+        System.out.println(temp);
+    }
+    static int debug=0;
+    private static void df(Object ob){ if(debug==1) System.out.print(ob); }
+    private static void dfn(Object ob){ if(debug==1) System.out.println(ob); }
+    private static void dfn(){ if(debug==1) System.out.println(); }
+    private static void dfn(int[] arr){
+        if(debug==1) {
+            StringBuilder temp = new StringBuilder();
+            for(int i=0;i<arr.length;i++){  temp.append(arr[i]).append(' ');  }
+            System.out.println(temp);
+        }
     }
 }
