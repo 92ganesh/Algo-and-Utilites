da7a6145983da4356061bff4c62796df7f230db6
Ganesh Patra <ganesh.patra@somaiya.edu>
2021-12-11 22:15:00 +0530

    fixed lcm method

--- utilities/Competitive Programming/lcm.java
+++ utilities/Competitive Programming/lcm.java
@@ -1,10 +1,12 @@
 // GCD by Euclidean algorithms
 // Time Complexity: O(Log min(a, b))
 
-public static int lcm(int a,int b,int MOD){
-    int gcd = gcd(a,b,MOD);
+public static long lcm(long a,long b){
+    long gcd = gcd(a,b);
+    long lcm = (a*b)/gcd;
+    return lcm;
 }
 
-public static int gcd(int a,int b){
+public static long gcd(long a,long b){
     return (b==0?a:gcd(b,a%b));
 }
