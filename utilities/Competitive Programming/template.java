da7523bd48fa38e149bfe6bdfc1c0cd18cb6d8ec
Ganesh Patra <ganesh.patra@somaiya.edu>
2022-01-29 19:58:46 +0530

    Update template.java

--- utilities/Competitive Programming/template.java
+++ utilities/Competitive Programming/template.java
@@ -1,6 +1,6 @@
 /*
 Credits:-
-1) Helper functions template from :- 
+1) Helper functions template from :-
 https://github.com/92ganesh/Algo-and-Utilites/blob/master/utilities/Competitive%20Programming/template.java
 */
 
@@ -11,29 +11,21 @@ class template {
     static BufferedReader br;
     public static void main(String[] args) throws Exception {
         try {
-            br = new BufferedReader(new FileReader("G:\\Workspace\\codechef\\custom.txt"));     debug=0;
+            br = new BufferedReader(new FileReader("F:\\Workspace\\CompetitiveProgramming\\input.txt"));  debug=true;
         } catch (Exception e) {
-            br = new BufferedReader(new InputStreamReader(System.in));      debug=0;
+            br = new BufferedReader(new InputStreamReader(System.in));      debug=false;
         }
         StringBuilder sb = new StringBuilder();
 
         int t = ri();
         while(t-- >0){
 
+
         }
-
-
-
     }
 
 
-
-
-
-
-
-
-    //Helper functions template from :- 
+    //Helper functions template from :-
     //https://github.com/92ganesh/Algo-and-Utilites/blob/master/utilities/Competitive%20Programming/template.java
     public static int MOD = (int)1e9+7;
     private static int ri() throws Exception{           return Integer.parseInt(br.readLine().trim());}
@@ -42,6 +34,14 @@ class template {
     private static long rl(String s){                   return Long.parseLong(s);}
     private static String rs() throws Exception{        return br.readLine();}
     private static String[] rss() throws Exception{     return br.readLine().trim().split("\\s+");}
+    private static int[] ass() throws Exception{
+        String[] ss = rss();
+        int[] arr = new int[ss.length];
+        for(int i=0;i<ss.length;i++){
+            arr[i]=ri(ss[i]);
+        }
+        return arr;
+    }
     private static void pf(Object ob){ System.out.print(ob);}
     private static void pfn(Object ob){System.out.println(ob);}
     private static void pfn(){System.out.println();}
@@ -55,12 +55,12 @@ class template {
         for(int i=0;i<arr.length;i++){  temp.append(arr[i]).append(' ');  }
         System.out.println(temp);
     }
-    static int debug=0;
-    private static void df(Object ob){ if(debug==1) System.out.print(ob); }
-    private static void dfn(Object ob){ if(debug==1) System.out.println(ob); }
-    private static void dfn(){ if(debug==1) System.out.println(); }
+    static boolean debug=false;
+    private static void df(Object ob){ if(debug) System.out.print(ob); }
+    private static void dfn(Object ob){ if(debug) System.out.println(ob); }
+    private static void dfn(){ if(debug) System.out.println(); }
     private static void dfn(int[] arr){
-        if(debug==1) {
+        if(debug) {
             StringBuilder temp = new StringBuilder();
             for(int i=0;i<arr.length;i++){  temp.append(arr[i]).append(' ');  }
             System.out.println(temp);
